8ecb07d6c139e5b691f3667bd909aea4c22495d6
avinashmohare <mohareavinash80@gmail.com>
2023-07-18 22:33:48 +0530

    Learnt Post Request

--- src/main/java/com/grades/gradesubmission/GradeController.java
+++ src/main/java/com/grades/gradesubmission/GradeController.java
@@ -1,21 +1,32 @@
 package com.grades.gradesubmission;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GradeController {
     
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Harry", "Potions", "c++"),
-        new Grade("Peter", "Science", "a++"),
-        new Grade("Parker", "Eng", "b++")
-    );
+    List<Grade> studentGrades = new ArrayList<>();  
+       
     
+    @GetMapping("/")
+    public String gradeForm(Model model){
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
+    
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade){
+        studentGrades.add(grade);
+        return "redirect:/grades";
+    }
+
     @GetMapping("/grades")
     public String getGrades(Model model){
         model.addAttribute("grades", studentGrades);
